6262e97b3a37e11ddfa917137e18427dd9927966
jdo227 <j.dong@utah.edu>
2024-11-03 21:17:35 -0700

    update time to utc

--- cms_missions.js
+++ cms_missions.js
@@ -1,186 +1,186 @@
 var cms_missions = [
-    {
-      name: "神舟五号",
-      mid: "sz5",
-      type: "crew",
-      start: "2003-10-15 09:00:00",
-      end: "2003-10-16 06:22:00",
-      crew: ["ylw"]
-    },
-    {
-      name: "神舟六号",
-      mid: "sz6",
-      type: "crew",
-      start: "2005-10-12 09:00:00",
-      end: "2005-10-17 04:33:00",
-      crew: ["fjl","nhs"]
-    },
-    {
-      name: "神舟七号",
-      mid: "sz7",
-      type: "crew",
-      start: "2008-09-25 21:10:00",
-      end: "2008-09-28 17:37:00",
-      crew: ["zzg", "lbm", "jhp"]
-    },
-    {
-      name: "神舟九号",
-      mid: "sz9",
-      type: "crew",
-      start: "2012-06-16 18:37:00",
-      end: "2012-06-29 10:03:00",
-      crew: ["jhp","lw","ly"]
-    },
-    {
-      name: "神舟十号",
-      mid: "sz10",
-      type: "crew",
-      start: "2013-06-11 17:38:00",
-      end: "2013-06-26 08:07:00",
-      crew: ["nhs","zxg","wyp"]
-    },
-    {
-      name: "神舟十一号",
-      mid: "sz11",
-      type: "crew",
-      start: "2016-10-17 07:30:00",
-      end: "2016-11-18 13:59:00",
-      crew: ["jhp","cd"]
-    },
-    {
-      name: "天和一号",
-      mid: "th-1",
-      type: "core",
-      start: "2021-04-29 11:23:17",
-      end: "now",
-      crew: ["NA"]
-    },
-    {
-      name: "天舟二号",
-      mid: "tz-2",
-      type: "cargo",
-      start: "2021-05-29 20:55:29",
-      end: "2022-03-31 18:40:00",
-      crew: ["NA"]
-    },
-    {
-      name: "神舟十二号",
-      mid: "sz12",
-      type: "crew",
-      start: "2021-06-17 09:22:00",
-      end: "2021-09-17 13:34:00",
-      crew: ["nhs", "lbm", "thb"]
-    },
-    {
-      name: "天舟三号",
-      mid: "tz-3",
-      type: "cargo",
-      start: "2021-09-20 15:10:11",
-      end: "2022-07-27 11:31:00",
-      crew: ["NA"]
-    },
-    {
-      name: "神舟十三号",
-      mid: "sz13",
-      type: "crew",
-      start: "2021-10-16 00:23:56",
-      end: "2022-04-16 09:56:00",
-      crew: ["zzg", "ygf", "wyp"]
-    },
-    {
-      name: "天舟四号",
-      mid: "tz-4",
-      type: "cargo",
-      start: "2022-05-10 01:56:37",
-      end: "2022-11-15 07:21:00",
-      crew: ["NA"]
-    },
-    {
-      name: "神舟十四号",
-      mid: "sz14",
-      type: "crew",
-      start: "2022-06-05 10:44:10",
-      end: "2022-12-04 20:09:00",
-      crew: ["cd", "ly", "cxz"]
-    },
-    {
-      name: "问天实验舱",
-      mid: "wentian",
-      type: "core",
-      start: "2022-07-24 14:22:23",
-      end: "now",
-      crew: ["NA"]
-    },
-    {
-      name: "梦天实验舱",
-      mid: "mengtian",
-      type: "core",
-      start: "2022-10-31 15:37:23",
-      end: "now",
-      crew: ["NA"]
-    },
-    {
-      name: "天舟五号",
-      mid: "tz-5",
-      type: "cargo",
-      start: "2022-11-12 10:03:12",
-      end: "2023-09-12 10:13:00",
-      crew: ["NA"]
-    },
-    {
-      name: "神舟十五号",
-      mid: "sz15",
-      type: "crew",
-      start: "2022-11-29 23:08:17",
-      end: "2023-06-04 06:32:58",
-      crew: ["fjl", "dqm", "zl"]
-    },
-    {
-      name: "天舟六号",
-      mid: "tz-6",
-      type: "cargo",
-      start: "2023-05-10 21:22:51",
-      end: "2024-01-19 20:37:00",
-      crew: ["NA"]
-    },
-    {
-      name: "神舟十六号",
-      mid: "sz16",
-      type: "crew",
-      start: "2023-05-30 09:31:13",
-      end: "2023-10-31 08:11:32",
-      crew: ["jhp", "zyz", "ghc"]
-    },
-    {
-      name: "神舟十七号",
-      mid: "sz17",
-      type: "crew",
-      start: "2023-10-26 11:14:02",
-      end: "2024-04-30 17:46:00",
-      crew: ["thb", "tsj", "jxl"]
-    },
-    {
-      name: "天舟七号",
-      mid: "tz-7",
-      type: "cargo",
-      start: "2024-01-17 22:27:30",
-      end: "now",
-      crew: ["NA"]
-    },
-    {
-      name: "神舟十八号",
-      mid: "sz18",
-      type: "crew",
-      start: "2024-04-25 20:59:00",
-      end: "2024-11-04 01:24:00",
-      crew: ["ygf", "lc", "lgs"]
-    },
-    {
-      name: "神舟十九号",
-      mid: "sz19",
-      type: "crew",
-      start: "2024-10-30 04:27:00",
-      end: "now",
-      crew: ["cxz", "sld", "whz"]
-    }    
-  ]
+  {
+    name: "神舟五号",
+    mid: "sz5",
+    type: "crew",
+    start: "2003-10-15T01:00:00Z",
+    end: "2003-10-15T22:22:00Z",
+    crew: ["ylw"]
+  },
+  {
+    name: "神舟六号",
+    mid: "sz6",
+    type: "crew",
+    start: "2005-10-12T01:00:00Z",
+    end: "2005-10-16T20:33:00Z",
+    crew: ["fjl", "nhs"]
+  },
+  {
+    name: "神舟七号",
+    mid: "sz7",
+    type: "crew",
+    start: "2008-09-25T13:10:00Z",
+    end: "2008-09-28T09:37:00Z",
+    crew: ["zzg", "lbm", "jhp"]
+  },
+  {
+    name: "神舟九号",
+    mid: "sz9",
+    type: "crew",
+    start: "2012-06-16T10:37:00Z",
+    end: "2012-06-29T02:03:00Z",
+    crew: ["jhp", "lw", "ly"]
+  },
+  {
+    name: "神舟十号",
+    mid: "sz10",
+    type: "crew",
+    start: "2013-06-11T09:38:00Z",
+    end: "2013-06-26T00:07:00Z",
+    crew: ["nhs", "zxg", "wyp"]
+  },
+  {
+    name: "神舟十一号",
+    mid: "sz11",
+    type: "crew",
+    start: "2016-10-16T23:30:00Z",
+    end: "2016-11-18T05:59:00Z",
+    crew: ["jhp", "cd"]
+  },
+  {
+    name: "天和一号",
+    mid: "th-1",
+    type: "core",
+    start: "2021-04-29T03:23:17Z",
+    end: "now",
+    crew: ["NA"]
+  },
+  {
+    name: "天舟二号",
+    mid: "tz-2",
+    type: "cargo",
+    start: "2021-05-29T12:55:29Z",
+    end: "2022-03-31T10:40:00Z",
+    crew: ["NA"]
+  },
+  {
+    name: "神舟十二号",
+    mid: "sz12",
+    type: "crew",
+    start: "2021-06-17T01:22:00Z",
+    end: "2021-09-17T05:34:00Z",
+    crew: ["nhs", "lbm", "thb"]
+  },
+  {
+    name: "天舟三号",
+    mid: "tz-3",
+    type: "cargo",
+    start: "2021-09-20T07:10:11Z",
+    end: "2022-07-27T03:31:00Z",
+    crew: ["NA"]
+  },
+  {
+    name: "神舟十三号",
+    mid: "sz13",
+    type: "crew",
+    start: "2021-10-15T16:23:56Z",
+    end: "2022-04-16T01:56:00Z",
+    crew: ["zzg", "ygf", "wyp"]
+  },
+  {
+    name: "天舟四号",
+    mid: "tz-4",
+    type: "cargo",
+    start: "2022-05-09T17:56:37Z",
+    end: "2022-11-14T23:21:00Z",
+    crew: ["NA"]
+  },
+  {
+    name: "神舟十四号",
+    mid: "sz14",
+    type: "crew",
+    start: "2022-06-05T02:44:10Z",
+    end: "2022-12-04T12:09:00Z",
+    crew: ["cd", "ly", "cxz"]
+  },
+  {
+    name: "问天实验舱",
+    mid: "wentian",
+    type: "core",
+    start: "2022-07-24T06:22:23Z",
+    end: "now",
+    crew: ["NA"]
+  },
+  {
+    name: "梦天实验舱",
+    mid: "mengtian",
+    type: "core",
+    start: "2022-10-31T07:37:23Z",
+    end: "now",
+    crew: ["NA"]
+  },
+  {
+    name: "天舟五号",
+    mid: "tz-5",
+    type: "cargo",
+    start: "2022-11-12T02:03:12Z",
+    end: "2023-09-12T02:13:00Z",
+    crew: ["NA"]
+  },
+  {
+    name: "神舟十五号",
+    mid: "sz15",
+    type: "crew",
+    start: "2022-11-29T15:08:17Z",
+    end: "2023-06-03T22:32:58Z",
+    crew: ["fjl", "dqm", "zl"]
+  },
+  {
+    name: "天舟六号",
+    mid: "tz-6",
+    type: "cargo",
+    start: "2023-05-10T13:22:51Z",
+    end: "2024-01-19T12:37:00Z",
+    crew: ["NA"]
+  },
+  {
+    name: "神舟十六号",
+    mid: "sz16",
+    type: "crew",
+    start: "2023-05-30T01:31:13Z",
+    end: "2023-10-31T00:11:32Z",
+    crew: ["jhp", "zyz", "ghc"]
+  },
+  {
+    name: "神舟十七号",
+    mid: "sz17",
+    type: "crew",
+    start: "2023-10-26T03:14:02Z",
+    end: "2024-04-30T09:46:00Z",
+    crew: ["thb", "tsj", "jxl"]
+  },
+  {
+    name: "天舟七号",
+    mid: "tz-7",
+    type: "cargo",
+    start: "2024-01-17T14:27:30Z",
+    end: "now",
+    crew: ["NA"]
+  },
+  {
+    name: "神舟十八号",
+    mid: "sz18",
+    type: "crew",
+    start: "2024-04-25T12:59:00Z",
+    end: "2024-11-03T17:24:00Z",
+    crew: ["ygf", "lc", "lgs"]
+  },
+  {
+    name: "神舟十九号",
+    mid: "sz19",
+    type: "crew",
+    start: "2024-10-29T20:27:00Z",
+    end: "now",
+    crew: ["cxz", "sld", "whz"]
+  }    
+];
